a6558c3508f8b0a65519090db3d05ef1f15a420e
u00t0 <sato0uta0@gmail.com>
2020-10-18 13:29:34 +0900

    main_ver13 拼音のルール設定

--- function.py
+++ function.py
@@ -4,17 +4,8 @@ import random
 import pinyin as pin
 from playsound import playsound
 from gtts import gTTS
-# https://pypi.org/project/pinyin/
 
-
-def no_num_pinyin(num_pinyin):  # str
-    re.findall("\D*\d", num_pinyin)
-    pinyin = num_pinyin
-    return pinyin
-
-
-
-def csv_to_mp3(filename):  # str
+def csv_to_mp3(filename): 
     with open('./imported_csv/'+filename+'.csv', 'r', encoding='utf-8_sig') as f:
         reader = csv.reader(f)
         vocab_list = [row for row in reader]
@@ -23,8 +14,7 @@ def csv_to_mp3(filename):  # str
             tts = gTTS(text=vocab_list[vocab_num]['zh_char'], lang='zh-cn')
             tts.save('./pronounce/'+filename+'/'+str(vocab_num)+'.mp3')
 
-
-def csv_shuffle_read(filename):  # str
+def csv_shuffle_read(filename):
     with open('./imported_csv/'+filename+'.csv', 'r', encoding='utf-8_sig') as f:
         reader = csv.reader(f)
         pure_list = [row for row in reader]
@@ -33,18 +23,8 @@ def csv_shuffle_read(filename):  # str
         vocab_list += [pure_list[num] for num in random_order]
     return vocab_list, random_order
 
-
-def csv_make(vocab_list, filename):  # VocabList, str
+def csv_make(vocab_list, filename):
     with open('./imported_csv/'+str(filename)+'.csv', 'x', encoding='utf-8_sig') as f:
         writer = csv.writer(f)
         for vocab_num in range(len(vocab_list)):
-            writer.writerow([vocab_list[vocab_num][0],
-                             vocab_list[vocab_num][1],
-                             vocab_list[vocab_num][2]])
-
-# vocab = VocabList()
-# vocab.input_zh_char()
-# vocab.check_pinyin()
-# vocab.input_meaning()
-# csv_make(vocab, 'test')
-# csv_to_mp3('test')
+            writer.writerow(vocab_list[vocab_num])
